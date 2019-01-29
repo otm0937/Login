a77ec5f9ceb7db248a1eaac8afb8d78f74480ced
chojake <sj203904@outlook.com>
2019-01-29 21:44:29 +0900

    Layout Edited

--- app/src/main/java/com/example/otm09/login/MainActivity.java
+++ app/src/main/java/com/example/otm09/login/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.otm09.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Make Android Stat Bar Transparent
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
 }
